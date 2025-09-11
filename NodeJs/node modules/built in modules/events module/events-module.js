a694633d79417f96862899aecea9a0975f86b067
Abdelmagidawad <abdelmagidawad@gmail.com>
2025-09-11 23:56:47 +0300

    upload events module

---
+++ NodeJs/node modules/built in modules/events module/events-module.js
@@ -0,0 +1,33 @@
+// *** Events Module  ***
+
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+//Register a Listener
+// emitter.addListener(event Name,callback fun) to register an event
+emitter.on("messageLogged1", () => {
+  console.log("Listener Called! Event One");
+});
+
+//raise an Event
+// emitter.emit(event Name) to raise an event
+emitter.emit("messageLogged1");
+
+// //*** Event Arguments
+// // Register a Listener
+emitter.on("messageLogged2", (arg) => {
+  console.log("Listener Called! Event Two", arg);
+  console.log("Listener Called! Event Two", arg.id);
+});
+//Raise an event
+emitter.emit("messageLogged2", { id: 1, url: "https:\\" });
+
+//*** Challenge Event Module
+//Raise event: Logging {data: message}
+
+// register a Listener
+emitter.on("logging", (arg) => {
+  console.log(`Hello User => ${arg}`);
+});
+// Raise an event
+emitter.emit("logging", "Login Successfully");
